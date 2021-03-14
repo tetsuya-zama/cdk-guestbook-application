f9b4c75e6091896777700bcc777467c74475bb29
tetsuya.zama <tetsuya.zama@gmail.com>
2021-03-15 06:51:06 +0900

    First valid e2e test

--- cypress/integration/rest_spec.ts
+++ cypress/integration/rest_spec.ts
@@ -4,13 +4,13 @@ interface AddPostRequest {
     image_file_name?: string
 }
 
-const API_BASE_URL = process.env["API_BASE_URL"];
+const API_BASE_URL = Cypress.env("API_BASE_URL");
 
 describe("Guest Book API", () => {
     const addPost = (post: AddPostRequest) => cy.request('POST', `${API_BASE_URL}`, post);
     const getPosts = () => cy.request('GET', `${API_BASE_URL}`);
     
-    test("add Post without image", () => {
+    it("add Post without image", () => {
         if(!API_BASE_URL) assert(false);
         
         const result = addPost({name: "Joe", message:"Hello, everyone!"});
@@ -20,7 +20,7 @@ describe("Guest Book API", () => {
             .should('include', 'application/json');
     });
     
-    test("get all Posts", () => {
+    it("get all Posts", () => {
         if(!API_BASE_URL) assert(false);
         
         const result = getPosts();
