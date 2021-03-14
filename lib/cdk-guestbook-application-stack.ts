4f62effafb03a7547599ae849d8e11249a17d4a4
EC2 Default User <ec2-user@ip-172-31-6-156.ap-northeast-1.compute.internal>
2021-03-14 04:49:43 +0000

    Add API Gateway

--- lib/cdk-guestbook-application-stack.ts
+++ lib/cdk-guestbook-application-stack.ts
@@ -1,9 +1,113 @@
 import * as cdk from '@aws-cdk/core';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3n from '@aws-cdk/aws-s3-notifications';
+import * as lmd from '@aws-cdk/aws-lambda';
+import * as lmdjs from '@aws-cdk/aws-lambda-nodejs';
+import * as agw from '@aws-cdk/aws-apigateway';
 
 export class CdkGuestbookApplicationStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const guestbookTable = new dynamodb.Table(this, 'guestbooktable', {
+      partitionKey: {name: 'h_key', type: dynamodb.AttributeType.STRING},
+      sortKey: {name: 's_key', type: dynamodb.AttributeType.STRING},
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+    });
+    
+    const imageFileBucket = new s3.Bucket(this, 'guestbook-images', {
+      cors : [{allowedOrigins: ["*"], allowedMethods: [s3.HttpMethods.POST, s3.HttpMethods.PUT], allowedHeaders:["*"]}],
+      publicReadAccess: true
+    });
+    
+    const lmdEnvironments = {
+      "GUEST_BOOK_TABLE_NAME": guestbookTable.tableName,
+      "FILE_BUCKET_NAME": imageFileBucket.bucketName,
+    }
+    
+    const commonLambdaSetting: Partial<lmd.FunctionProps> = {
+      environment: lmdEnvironments,
+      tracing: lmd.Tracing.ACTIVE,
+      timeout: cdk.Duration.minutes(3)
+    }
+    
+    const getIndexJs = new lmdjs.NodejsFunction(this, 'get-index-js', {
+      entry: "./lib/guestbook/resthandlers.ts",
+      handler: "getIndexHandler",
+      ...commonLambdaSetting
+    });
+    
+    guestbookTable.grantReadData(getIndexJs);
+    
+    const getPostJs = new lmdjs.NodejsFunction(this, 'get-post-js', {
+      entry: "./lib/guestbook/resthandlers.ts",
+      handler: "getPostHandler",
+      ...commonLambdaSetting
+    });
+    
+    
+    guestbookTable.grantReadData(getPostJs);
+    
+    const postIndexJs = new lmdjs.NodejsFunction(this, 'post-index-js', {
+      entry: "./lib/guestbook/resthandlers.ts",
+      handler: "postIndexHandler",
+      ...commonLambdaSetting
+    });
+    
+    
+    guestbookTable.grantReadWriteData(postIndexJs);
+    imageFileBucket.grantWrite(postIndexJs);
+    
+    const postReplyJs = new lmdjs.NodejsFunction(this, 'post-reply-js', {
+      entry: "./lib/guestbook/resthandlers.ts",
+      handler: "postReplyHandler",
+      ...commonLambdaSetting
+    });
+    
+    guestbookTable.grantReadWriteData(postReplyJs);
+    
+    const deletePostJs = new lmdjs.NodejsFunction(this, 'delete-post-js', {
+      entry: "./lib/guestbook/resthandlers.ts",
+      handler: "deletePostHandler",
+      ...commonLambdaSetting
+    });
+    
+    guestbookTable.grantReadWriteData(deletePostJs);
+    
+    const onImageUploaded = new lmdjs.NodejsFunction(this, 'on-image-uploaded', {
+      entry: "./lib/guestbook/s3handlers.ts",
+      handler: "imageUploadedHandler",
+      ...commonLambdaSetting
+    });
+    
+    guestbookTable.grantReadWriteData(onImageUploaded);
+    imageFileBucket.addEventNotification(s3.EventType.OBJECT_CREATED ,new s3n.LambdaDestination(onImageUploaded));
+    
+    const api = new agw.RestApi(this, 'guestbook-api', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: agw.Cors.ALL_ORIGINS,
+        allowMethods: agw.Cors.ALL_METHODS
+      },
+      deployOptions: {
+        tracingEnabled: true
+      }
+    });
+    
+    
+    const root = api.root;
+    root.addMethod('GET', new agw.LambdaIntegration(getIndexJs));
+    root.addMethod('POST', new agw.LambdaIntegration(postIndexJs));
+
+    const posts = root.addResource('posts');
+    posts.addMethod('GET', new agw.LambdaIntegration(getIndexJs));
+    posts.addMethod('POST', new agw.LambdaIntegration(postIndexJs));
+
+    const postsPostId = posts.addResource('{postid}');
+    postsPostId.addMethod('GET', new agw.LambdaIntegration(getPostJs));
+    postsPostId.addMethod('DELETE', new agw.LambdaIntegration(deletePostJs));
+
+    const jsPostsPostIdReplies = postsPostId.addResource('replies');
+    jsPostsPostIdReplies.addMethod('POST', new agw.LambdaIntegration(postReplyJs));
   }
 }
--- test/cdk-guestbook-application.test.ts
+++ test/cdk-guestbook-application.test.ts
@@ -1,13 +1,86 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import "@aws-cdk/assert/jest";
 import * as cdk from '@aws-cdk/core';
 import * as CdkGuestbookApplication from '../lib/cdk-guestbook-application-stack';
 
-test('Empty Stack', () => {
+test('REST API Stack', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new CdkGuestbookApplication.CdkGuestbookApplicationStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expect(stack).toHaveResourceLike("AWS::DynamoDB::Table", {
+      "KeySchema": [
+        {
+          "AttributeName": "h_key",
+          "KeyType": "HASH"
+        },
+        {
+          "AttributeName": "s_key",
+          "KeyType": "RANGE"
+        }
+      ],
+      "AttributeDefinitions": [
+        {
+          "AttributeName": "h_key",
+          "AttributeType": "S"
+        },
+        {
+          "AttributeName": "s_key",
+          "AttributeType": "S"
+        }
+      ],
+      "BillingMode": "PAY_PER_REQUEST"
+    });
+    
+    expect(stack).toHaveResourceLike("AWS::S3::Bucket", {
+      "CorsConfiguration": {
+          "CorsRules": [
+            {
+              "AllowedHeaders": [
+                "*"
+              ],
+              "AllowedMethods": [
+                "POST",
+                "PUT"
+              ],
+              "AllowedOrigins": [
+                "*"
+              ]
+            }
+          ]
+        }
+    });
+    
+    expect(stack).toHaveResource("AWS::Lambda::Function", {
+        "Handler": "index.getIndexHandler",
+        "Runtime": "nodejs12.x"
+    });
+    
+    expect(stack).toHaveResource("AWS::Lambda::Function", {
+        "Handler": "index.getPostHandler",
+        "Runtime": "nodejs12.x"
+    });
+    
+    expect(stack).toHaveResource("AWS::Lambda::Function", {
+        "Handler": "index.postIndexHandler",
+        "Runtime": "nodejs12.x"
+    });
+    
+    expect(stack).toHaveResource("AWS::Lambda::Function", {
+        "Handler": "index.postReplyHandler",
+        "Runtime": "nodejs12.x"
+    });
+    
+    expect(stack).toHaveResource("AWS::Lambda::Function", {
+        "Handler": "index.deletePostHandler",
+        "Runtime": "nodejs12.x"
+    });
+    
+    expect(stack).toHaveResource("AWS::Lambda::Function", {
+        "Handler": "index.imageUploadedHandler",
+        "Runtime": "nodejs12.x"
+    });
+    
+    expect(stack).toHaveResourceLike("AWS::ApiGateway::RestApi", {
+      "Name": "guestbook-api"
+    });
 });
